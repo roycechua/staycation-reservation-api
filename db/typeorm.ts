019f3d8226a38b6fdca15bc8b105a8d26564f514
Royce <royce236@gmail.com>
2024-10-29 01:43:48 +0800

    typeorm host config - make it point to docker container db

--- db/typeorm.ts
+++ db/typeorm.ts
@@ -2,7 +2,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 export default TypeOrmModule.forRoot({
   type: 'postgres',
-  host: 'localhost',
+  host: 'db',
   port: 5432,
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
